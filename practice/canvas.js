a4d281fd4e2dee38358dce39c4db9301b33dc9a1
Your Name <you@example.com>
2022-11-15 15:43:41 -0700

    Friday

--- practice/canvas.js
+++ practice/canvas.js
@@ -1,25 +1,92 @@
-//let canvas = document.getElementsByClassId("canvas-1");
-/** @type{canvastRenderingcantext2D}*/
-//let ctx = canvas.getcontex("
+//@ts-check
+/** @type {HTMLCanvasElement} */ //@ts-ignore
+let canvas = document.getElementById("canvas-1");
+canvas.width = 100;
+canvas.height = 100;
 
-//ctx.fillstyle = "purple";
-//ctx.fillreact(250,250,190,190);
+/** @type {CanvasRenderingContext2D} */ //@ts-ignore
+let context = canvas.getContext("2d");
 
-//Context.fillstyle= "purple";
-//context.fillreact(50,0,25,25);
-ctx.fillstyle = "red";
-let context.fillstyle= canvas.getcontex("2d");
-let squares ={};
 
-Drawsqaure(0,0 "yellow")
-Function darwsquare(x, y, color, size){
-   let square = new Path2D();
-   square.rect( x , y , size, size);
 
-   squares.push(square);
-	context.fillstlye = color
+class ClickBox {
+	constructor(x, y, size, colors) {
+		this.x = x;
+		this.y = y;
+		this.size = size;
 
-  context.fillreact(x,y,size,size);
+		this.isClicked = false;
+		this.refreshRate = 500;
+		this.lastRefresh = 0;
+		this.colors = colors;
+		this.color = "red";
+
+		this.setColor();
+	}
+
+	setColor() {
+		let colorIndex = Math.floor(Math.random() * this.colors.length);
+		this.color = this.colors[colorIndex];
+	}
+
+	update(timeElapsed) {
+		this.lastRefresh += timeElapsed;
+
+		if(this.lastRefresh < this.refreshRate) return;
+
+
+		if(this.lastRefresh >= this.refreshRate) {
+			this.setColor();
+		}
+	}
+
+	draw() {}
 }
 
-console.log(square);
+let squares = [];
+let gridSize = 4;
+let size = canvas.width / gridSize;
+let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+function drawSquare(x, y, color, size = 25) {
+	let square = new Path2D();
+	square.rect(x, y, size, size);
+
+	squares.push(square);
+
+	context.fillStyle = color;
+	context.fillRect(x, y, size, size);
+}
+
+console.log(squares);
+
+let currentTime = 0;
+const refreshRate = 500;
+let lastRefresh = 0;
+
+function drawLoop(timestamp) {
+	let elapsedTime = timestamp - currentTime;
+	currentTime = timestamp;
+
+	lastRefresh = lastRefresh + elapsedTime;
+
+	if (lastRefresh >= refreshRate) {
+		lastRefresh = 0;
+
+		// All of these incrementors do the same
+		// row = row + 1
+		// row +=  1
+		// row++
+		for (let row = 0; row < gridSize; row++) {
+			for (let col = 0; col < gridSize; col++) {
+				let colorIndex = Math.floor(Math.random() * colors.length);
+				let color = colors[colorIndex];
+				drawSquare(col * size, row * size, color, size);
+			}
+		}
+	}
+
+	requestAnimationFrame(drawLoop);
+}
+
+requestAnimationFrame(drawLoop);
