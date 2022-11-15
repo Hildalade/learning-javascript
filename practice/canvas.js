7eac027c13a2d3575051682c3f7da07f69478c74
Your Name <you@example.com>
2022-11-15 16:20:42 -0700

    tuesday

--- practice/canvas.js
+++ practice/canvas.js
@@ -25,68 +25,57 @@ class ClickBox {
 	}
 
 	setColor() {
-		let colorIndex = Math.floor(Math.random() * this.colors.length);
-		this.color = this.colors[colorIndex];
-	}
+  
+ let x= column * size 
+ let y= row * size 
+ let box = new ClickBox()
+ squares.push(box);
+}
 
 	update(timeElapsed) {
 		this.lastRefresh += timeElapsed;
 
 		if(this.lastRefresh < this.refreshRate) return;
 
-
-		if(this.lastRefresh >= this.refreshRate) {
-			this.setColor();
+    this.lastRefresh = 0;
+    this.setColor();
 		}
 	}
 
-	draw() {}
-}
+	Draw() {
+    //let square = new path2d();
+    //square.rect(x,y,size,size);
+    context.fillStyle = this.color;
+    context.fillRect(this.y,this.x, this.size,this.size)
+  }
+
 
 let squares = [];
 let gridSize = 4;
 let size = canvas.width / gridSize;
 let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
-function drawSquare(x, y, color, size = 25) {
-	let square = new Path2D();
-	square.rect(x, y, size, size);
-
-	squares.push(square);
-
-	context.fillStyle = color;
-	context.fillRect(x, y, size, size);
-}
+// function drawSquare(x, y, color, size = 25) {
+// //let square = new path2d();
+//     //square.rect(x,y,size,size);
+// 	context.fillStyle = color;
+// 	context.fillRect(x, y, size, size);
+// }
 
 console.log(squares);
 
 let currentTime = 0;
-const refreshRate = 500;
-let lastRefresh = 0;
 
 function drawLoop(timestamp) {
 	let elapsedTime = timestamp - currentTime;
 	currentTime = timestamp;
 
-	lastRefresh = lastRefresh + elapsedTime;
+  squares.foreach((b) =>){
+    b.update(elapsedTime);
+    b.draw();
+  });
 
-	if (lastRefresh >= refreshRate) {
-		lastRefresh = 0;
-
-		// All of these incrementors do the same
-		// row = row + 1
-		// row +=  1
-		// row++
-		for (let row = 0; row < gridSize; row++) {
-			for (let col = 0; col < gridSize; col++) {
-				let colorIndex = Math.floor(Math.random() * colors.length);
-				let color = colors[colorIndex];
-				drawSquare(col * size, row * size, color, size);
-			}
-		}
-	}
-
-	requestAnimationFrame(drawLoop);
+  requestAnimationFrame(drawLoop);
 }
 
 requestAnimationFrame(drawLoop);
