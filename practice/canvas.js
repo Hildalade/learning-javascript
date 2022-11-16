7175a402da3ec9f55c66e487e2468384d7e3bb5b
Your Name <you@example.com>
2022-11-16 15:40:42 -0700

    working squares

--- practice/canvas.js
+++ practice/canvas.js
@@ -7,10 +7,8 @@ canvas.height = 100;
 /** @type {CanvasRenderingContext2D} */ //@ts-ignore
 let context = canvas.getContext("2d");
 
-
-
 class ClickBox {
-	constructor(x, y, colors, size) {
+	constructor(x, y, size, colors) {
 		this.x = x;
 		this.y = y;
 		this.size = size;
@@ -25,42 +23,44 @@ class ClickBox {
 	}
 
 	setColor() {
-    let colorindex = Math.floor (Math.random() * this.colors.length);
-    let color = colors[colorindex];
-}
+		let colorIndex = Math.floor(Math.random() * this.colors.length);
+		this.color = this.colors[colorIndex];
+	}
 
 	update(timeElapsed) {
 		this.lastRefresh += timeElapsed;
 
-		if(this.lastRefresh < this.refreshRate) return;
+		if (this.lastRefresh < this.refreshRate) return;
 
-    this.lastRefresh = 0;
-    this.setColor();
-		}
-	
+		// reset the last refresh counter
+		this.lastRefresh = 0;
+
+		// set a new random color to myself
+		this.setColor();
+	}
 
 	draw() {
-    //let square = new path2d();
-    //square.rect(x,y,size,size);
-    context.fillStyle = this.color;
-    context.fillRect(this.y,this.x, this.size,this.size);
-  }
-}
+		// let square = new Path2D();
+		// square.rect(x, y, size, size);
 
+		context.fillStyle = this.color;
+		context.fillRect(this.x, this.y, this.size, this.size);
+	}
+}
 
 let squares = [];
 let gridSize = 4;
 let size = canvas.width / gridSize;
 let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
-// function drawSquare(x, y, color, size = 25) {
-// //let square = new path2d();
-//     //square.rect(x,y,size,size);
-// 	context.fillStyle = color;
-// 	context.fillRect(x, y, size, size);
-// }
-
-console.log(squares);
+for (let row = 0; row < gridSize; row++) {
+	for (let col = 0; col < gridSize; col++) {
+		let x = col * size;
+		let y = row * size;
+		let box = new ClickBox(x, y, size, colors);
+		squares.push(box);
+	}
+}
 
 let currentTime = 0;
 
@@ -68,12 +68,12 @@ function drawLoop(timestamp) {
 	let elapsedTime = timestamp - currentTime;
 	currentTime = timestamp;
 
-  squares.forEach((b) =>{
-    b.update(elapsedTime);
-    b.draw();
-  });
+	squares.forEach((b) => {
+		b.update(elapsedTime);
+		b.draw();
+	});
 
-  requestAnimationFrame(drawLoop);
+	requestAnimationFrame(drawLoop);
 }
 
 requestAnimationFrame(drawLoop);
